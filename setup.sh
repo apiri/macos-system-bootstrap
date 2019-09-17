c63a2475d23335213b5f37df5b4f1479cf53799e
Aldrin Piri <aldrin@apache.org>
2019-09-17 14:18:57 -0400

    Provide minimal Mac setup and setup Homebrew & Oh My Zsh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# Make scrolling actually natural
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+## VNC
+sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+sudo defaults write /Library/Preferences/com.apple.RemoteManagement.plist VNCOnlyLocalConnections -bool no
+
+# Bounce Finder to enable preferences
+killall Finder
+
+# Setup homebrew if it is not installed.  This will also install XCode development tools
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew already installed..."
+fi
+
+# Apply configuration
+## Oh My Zsh
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh already initialized..."
+fi
